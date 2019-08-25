39c2cc63c78ee2e8ed9f4ddfa97cc4f9b2371acf
zhangbicheng <zhangbicheng@zhangdeMacBook-Air.local>
2019-08-25 12:49:44 +0800

    modify demo

--- crawler.py
+++ crawler.py
@@ -1,230 +1,73 @@
-# just for 【热门微博】 on mobile web
-import time
-import random
-import os
-from os import path
 import requests
-import pickle
-# from pyquery import PyQuery
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import logging
+import base64
+import json
 
-"""表单数据说明
-password:        输入的密码，明文 #我是用手机号登录，邮箱应该也可以
-savestate:      好像只能是 1
-r:      refer  # http://m.weibo.cn/ # 应该是最开始的触发网址，一般都是这个
-ec:         ERROR_COUNT # 帐号/密码出错次数
-pagerefer:      正常的refere, 但注意移动页面和PC页面的不同
-entry:      入口 # mweibo
-wentry:     和上一个配套 默认空 # 可能和不同的登录页面有关
-loginfrom:      默认空，但是属性被隐藏了
-client_id:      默认空，因为我是浏览器？或者第三方登录才会有
-code:       空
-qq:     空
-mainpageflag:   1   
-hff:        支付免登录流程 红包飞新增
-hfp:        支付免登录流程 红包飞新增"""
-
-class WBCrawler(object):
-
-    def __init__(self, maximum=2):
-
-        self.session = self._load_session() or requests.session()
-        self.cards_count = maximum # pages' count of hot posts
-        self.HOST = 'https://m.weibo.cn'
-        self.HOT_POST_URL = 'https://m.weibo.cn/api/container/getIndex?containerid=102803'
-        self.data = {
-            'username': 'account(phone number)', # 账号
-            'password': 'plaintext-password', # 密码
-            'savestate': '1', 
-            'r': 'http://m.weibo.cn/', # 触发网址
-            'ec': '0', # 账号、密码出错次数
-            'pagerefer': '', 
-            'entry': 'mweibo', # 入口
-            'wentry': '', 'loginfrom': '', 
-            'client_id': '', # 默认空，因为我是浏览器？或者第三方登录才会有
-            'code': '', 
-            'qq': '', 
-            'mainpageflag': '1',
-            'hff': '', 
-            'hfp': ''
-        }
-        self.postid = []
-        self.poster = []
-        self.texts = []
-        self.signs = []
-        self.comments = []
-
-    def _load_session(self):
-        '''从path中获取session'''
-        if path.exists('session'):
-            with open('session', 'rb') as fr:
-                session = pickle.load(fr)
-                print('load session from cache')
-            return session
-        else:
-            return None
-
-    def need_to_login(self):
-        '''判断是否需要登陆'''
-        resp = self.session.get(self.HOST)
-        if 'passport' in resp.url or 'login' in resp.url:
-            print('need to login')
-            return True
-        else:
-            return False
-
-    def _find_referer_url(self, text):
-        doc = PyQuery(text)
-        a_tag = doc.find('.action a').eq(1)
-        return a_tag.attr('href')
-
-    def login(self):
-        '''登陆'''
-        url = 'http://m.weibo.cn'
-        post_url = 'https://passport.weibo.cn/sso/login'
-        self.session.headers.update({
-            'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Mobile Safari/537.36'
-        })
-        resp = self.session.get(url)
-        self.session.headers.update({'Referer': self._find_referer_url(resp.text)})
-        resp = self.session.post(post_url, data=self.data)
-        if resp.status_code == 200:
-            print('signed in')
-            with open('session', 'wb') as fw:
-                pickle.dump(self.session, fw)
-        else:
-            print('login failed')
-
-    def get_hot_cards(self):
-        '''获取热门'''
-        resp = self.session.get(self.HOT_POST_URL)
-        cur = 1
-        while cur < self.cards_count:
-            yield resp.json()['cards']
-            resp = self.session.get(self.HOT_POST_URL, params={'since_id': cur})
-            cur += 1
-
-    def _is_short_text(self, data:str):
-        if PyQuery(data).find('a:contains(全文)'):
-            return False
-        else:
-            return True
-
-    def _clear_html(self, data:str):
-        return PyQuery(data).text()
-
-    def _get_poster(self, data):
-        return str(data['id']), data['screen_name'], str(data['verified'])
-
-    def _get_text(self, data):
-        if data:
-            if 'longTextContent' in data:
-                return self._clear_html(data['longTextContent'])
-            else:
-                return self._clear_html(data['text'])
-
-    def _get_signs(self, data=None):
-        if data:
-            return data['reposts_count'], data['comments_count'], data['attitudes_count']
-
-    def _get_comments(self, data):
-        # always get the first page of comments
-        # Default to get hot comments. If there are not them, to get normal comments
-        if not data["ok"]:
-            print('comments can be loaded')
-            return []
-        comments = data['hot_data'] if 'hot_data' in data else data['data']
-        _hcl = list()
-        for item in comments:
-            _c = dict(
-                user=item['user']['id'], verified=item['user']['verified'],
-                content=self._clear_html(item['text']), likers=item['like_counts'])
-            _hcl.append(_c)
-        return _hcl
-
-    def _sleep(self):
-        count = random.randrange(1, 4) + random.random()
-        time.sleep(count)
-
-    def _get_post_url_json(self, c_url=None, t_url=None):
-        self._sleep()
-        c_resp = c_url and self.session.get(c_url).json()
-        t_resp = t_url and self.session.get(t_url).json()
-        return c_resp, t_resp
-
-    def parser_cards(self, data: list):
-        POST_PATH = 'https://m.weibo.cn/statuses/extend?id={0}'
-        COMMENTS_PATH = 'https://m.weibo.cn/api/comments/show?id={0}&page=1'
-        for card in data:
-            user = card['mblog']['user']
-            self.poster.append(self._get_poster(user))
-            id = card['mblog']['idstr']
-            self.postid.append(id)
-            if self._is_short_text(card['mblog']['text']):
-                self.texts.append(self._get_text(card['mblog']))
-                self.signs.append(self._get_signs(card['mblog']))
-                c, t = self._get_post_url_json(c_url=COMMENTS_PATH.format(id))
-                self.comments.append(self._get_comments(c))
-            else:
-                c, t = self._get_post_url_json(c_url=COMMENTS_PATH.format(id), t_url=POST_PATH.format(id))
-                self.texts.append(self._get_text(t))
-                self.signs.append(self._get_signs(t))
-                self.comments.append(self._get_comments(c))
-
-    def make_obj(self):
-        print('make_obj')
-        for i in range(len(self.poster)):
-            obj = {
-                'poster': self.poster[i], # (id, name, verified)
-                'text': self.texts[i], # str
-                'signs': self.signs[i], # (forward_count#comment_count#liker_count)
-                'comments': self.comments[i] # [{user-id, user-verified, content, liker-count}, ...]
-            }
-            yield obj, self.postid[i]
-
-    def save(self, obj, num):
-        '''保存爬取结果'''
-        name = 'weibo_file/weibo_data_%s' % num
-        with open(name, 'wb') as fw:
-            pickle.dump(obj, fw)
-            print('Save %s' % name)
-
-    def start(self):
-        '''开始爬取'''
-        print('start')
-        if self.need_to_login():
-            self.login()
-        for cards in self.get_hot_cards():
-            self.parser_cards(cards)
-        print('save...')
-        for obj, pid in self.make_obj():
-            self.save(obj, pid)
-        print('all finished.')
-
-    def __del__(self):
-        with open('session', 'wb') as fw:
-            pickle.dump(self.session, fw)
-            print('refresh session')
+IDENTIFY = 1  # 验证码输入方式:        1:看截图aa.png，手动输入     2:云打码
+COOKIE_GETWAY = 0 # 0 代表从https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18) 获取cookie   # 1 代表从https://weibo.cn/login/获取Cookie
+dcap = dict(DesiredCapabilities.PHANTOMJS)  # PhantomJS需要使用老版手机的user-agent，不然验证码会无法通过
+dcap["phantomjs.page.settings.userAgent"] = (
+    "Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
+)
+logger = logging.getLogger(__name__)
+logging.getLogger("selenium").setLevel(logging.WARNING)  # 将selenium的日志级别设成WARNING，太烦人
 
 
-def show_random_data():
-    '''随机检查'''
-    files = os.listdir('./weibo_file/')
-    name = random.choice(files)
-    with open('./weibo_file/'+name, 'rb') as fr:
-        obj = pickle.load(fr)
-        print(name)
-        print('-'*25)
-        for k,v in obj.items():
-            print(k)
-            print(v)
-            print('-'*25)
+"""
+输入你的微博账号和密码，可去淘宝买。
+建议买几十个，微博限制的严，太频繁了会出现302转移。
+或者你也可以把时间间隔调大点。
+"""
+myWeiBo = [
+    {'no': 'jiadieyuso3319@163.com', 'psw': 'a123456'},
+    {'no': 'shudieful3618@163.com', 'psw': 'a123456'},
+]
+
+def get_cookie_from_login_sina_com_cn(account, password):
+    '''获取一个账号的cookie'''
+    loginURL = "https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18)"
+    username = base64.b64encode(account.encode("utf-8")).decode("utf-8")
+    postData = {
+        "entry": "sso",
+        "gateway": "1",
+        "from": "null",
+        "savestate": "30",
+        "useticket": "0",
+        "pagerefer": "",
+        "vsnf": "1",
+        "su": username,
+        "service": "sso",
+        "sp": password,
+        "sr": "1440*900",
+        "encoding": "UTF-8",
+        "cdult": "3",
+        "domain": "sina.com.cn",
+        "prelt": "0",
+        "returntype": "TEXT",
+    }
+    session = requests.Session()
+    r = session.post(loginURL, data=postData)
+    jsonStr = r.content.decode("gbk")
+    info = json.loads(jsonStr)
+    if info["retcode"] == "0":
+        logger.warning("Get Cookie Success!( Account:%s )" % account)
+        cookie = session.cookies.get_dict()
+        return json.dumps(cookie)
+    else:
+        logger.warning("Failed!( Reason:%s )" % info["reason"])
+        return ""
 
 
-if __name__ == '__main__':
-    # wb = WBCrawler()
-    # wb.start()
-    # show_random_data()
-    r = requests.options('https://api.github.com/events')
-    x = requests.session()
-    print(x)
-    print(r)
+def getCookie(account, password):
+    if COOKIE_GETWAY == 0:
+        return get_cookie_from_login_sina_com_cn(account, password)
+    elif COOKIE_GETWAY == 1:
+        return get_cookie_from_weibo_cn(account, password)
+    else:
+        logger.error("COOKIT_GETWAY Error!")
+
+if __name__ == "__main__":
+    nick_name = ''
+    password = ''
+    print(getCookie(nick_name, password))
