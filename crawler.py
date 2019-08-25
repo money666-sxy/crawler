2b3d6985d67151eac6f82d530892122ce9084b60
zhangbicheng <zhangbicheng@zhangdeMacBook-Air.local>
2019-08-25 23:32:35 +0800

    add get_cookie_from_weibo_cn

--- crawler.py
+++ crawler.py
@@ -5,13 +5,16 @@ import base64
 import json
 
 IDENTIFY = 1  # 验证码输入方式:        1:看截图aa.png，手动输入     2:云打码
-COOKIE_GETWAY = 0 # 0 代表从https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18) 获取cookie   # 1 代表从https://weibo.cn/login/获取Cookie
-dcap = dict(DesiredCapabilities.PHANTOMJS)  # PhantomJS需要使用老版手机的user-agent，不然验证码会无法通过
+# 0 代表从https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18) 获取cookie   # 1 代表从https://weibo.cn/login/获取Cookie
+COOKIE_GETWAY = 0
+# PhantomJS需要使用老版手机的user-agent，不然验证码会无法通过
+dcap = dict(DesiredCapabilities.PHANTOMJS)
 dcap["phantomjs.page.settings.userAgent"] = (
     "Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
 )
 logger = logging.getLogger(__name__)
-logging.getLogger("selenium").setLevel(logging.WARNING)  # 将selenium的日志级别设成WARNING，太烦人
+logging.getLogger("selenium").setLevel(
+    logging.WARNING)  # 将selenium的日志级别设成WARNING，太烦人
 
 
 """
@@ -23,6 +26,7 @@ myWeiBo = [
     {'no': 'jiadieyuso3319@163.com', 'psw': 'a123456'},
     {'no': 'shudieful3618@163.com', 'psw': 'a123456'},
 ]
+
 
 def get_cookie_from_login_sina_com_cn(account, password):
     '''获取一个账号的cookie'''
@@ -67,7 +71,13 @@ def getCookie(account, password):
     else:
         logger.error("COOKIT_GETWAY Error!")
 
+
+def get_cookie_from_weibo_cn(account, password):
+    pass
+
+
 if __name__ == "__main__":
     nick_name = ''
     password = ''
     print(getCookie(nick_name, password))
+    a =10
